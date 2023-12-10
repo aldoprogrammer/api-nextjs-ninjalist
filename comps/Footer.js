4f3b1bd28de349a2c5ab4605b7c2b9dfb851de53
aldoprogrammer <aldobesma@gmail.com>
2023-12-10 12:27:18 +0700

    first commit

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            Copyright 2023 Aldo L.S.
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const Ninjas = () => {
+    return ( 
+        <>
+            <h1>See All Ninjas</h1>
+        </>
+     );
+}
+ 
+export default Ninjas;
